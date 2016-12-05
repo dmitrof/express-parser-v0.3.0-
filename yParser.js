0b53ed3cdbd4c3068b09119486fc6ced4de7f616
Дмитрий <ttmitry@gmail.com>
2016-12-05 21:37:21 +0300

    now using express js

--- yParser.js
+++ yParser.js
@@ -1,4 +1,19 @@
 /**
- * Created by Дмитрий on 05.12.2016.
+ * Created by Дмитрий on 23.11.2016.
  */
 //api key is AIzaSyA1faslXR56EAbtZz9LFHsUtZeYXr4SOTw
+
+var YouTube = require('youtube-node');
+
+var youTube = new YouTube();
+
+youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
+
+youTube.search('World War z Trailer', 2, function(error, result) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(JSON.stringify(result, null, 2));
+    }
+});
