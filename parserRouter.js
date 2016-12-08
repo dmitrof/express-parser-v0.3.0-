bae4d0e68312a6f33adc93e64071ad0f66e425da
Дмитрий <ttmitry@gmail.com>
2016-12-08 19:12:28 +0300

    now using express js

--- parserRouter.js
+++ parserRouter.js
@@ -14,17 +14,30 @@ module.exports.setDB = function(_db) {
 module.exports.parserRouter = {
     source_types : require('./source_types'),
     //adding source to database. URL validation required
-    addSource : function(source, source_type, callback) {
-        validateParser(source_type, function(err, result) {
-
+    addSource : function(source_url, source_type, callback) {
+        var sourceAddedPromises = [];
+        var parser;
+        p = new Promise(function(resolve, reject) {
+            this.getParser(source_url, source_type, function(err, _parser) {
+                if (err) {console.log(err); reject('source was not validated')}
+                parser = _parser;
+                resolve('source is validated')
+            })
         });
+
+
+
+
     },
-    //validate URL here
-    validateParser : function(parserInfo, callback) {
+    //validate URL and source type here, get parser
+    getParser : function(source_url, source_type, callback) {
 
     },
     //parse start parsing source contents. Delegates logic to specific parser
     parseSource : function(source, callback) {
+
+    },
+    insertSourceToDB : function(source_url, source_info, callback) {
 
     }
 
