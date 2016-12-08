074b8e895df10d553c0b0cf918e50736091fe31a
Дмитрий <ttmitry@gmail.com>
2016-12-08 19:34:56 +0300

    now using express js

--- parserRouter.js
+++ parserRouter.js
@@ -12,7 +12,9 @@ module.exports.setDB = function(_db) {
 };
 
 module.exports.parserRouter = {
-    source_types : require('./source_types'),
+    sources_types : require('./source_types'),
+    sources_urls : require('./source_url'),
+
     //adding source to database. URL validation required
     addSource : function(source_url, source_type, callback) {
         var sourceAddedPromises = [];
@@ -24,6 +26,7 @@ module.exports.parserRouter = {
                 resolve('source is validated')
             })
         });
+        p.then();
 
 
 
@@ -31,13 +34,32 @@ module.exports.parserRouter = {
     },
     //validate URL and source type here, get parser
     getParser : function(source_url, source_type, callback) {
+        var parser;
+        if (source_type != null) {  //get parser from source_type
+            parserType = this.source_types[source_type];
+            callback(null, parser);
+            var urls_list = this.sources_urls[parserType];
+            //доделать ДОМЕН
+            if (urls_list.indexOf(source_url) != -1) {
+                parser = parserType;
+                callback(null, parser);
+            }
+            else {callback('source URL is not valid')}
 
+
+        }
     },
-    //parse start parsing source contents. Delegates logic to specific parser
+    //parse start parsing source's contents. Delegates logic to specific parser (for exmple jishoParser)
     parseSource : function(source, callback) {
 
     },
     insertSourceToDB : function(source_url, source_info, callback) {
+
+    },
+    validateUrl : function(source_url,callback) {
+
+    },
+    getUrlDomain : function(source_url, callback) {
 
     }
 
